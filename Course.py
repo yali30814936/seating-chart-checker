cf80c4bf4fb1454d64af72adbe548504a15d9c71
UBIN <1213121abc@gmail.com>
2023-05-19 00:40:41 +0800

    feat: 初始化學生名單與出席紀錄
    3個學生、2個出席紀錄

--- Course.py
+++ Course.py
@@ -8,8 +8,22 @@ class Course:
         self._name = name
         self._ID = ID
 
-        self._student_list = []
-        self._rollcall_records = dict()
+        self._student_list = [
+            Student("Steven", "00000001", "資工系"),
+            Student("Amelia", "00000002", "資工系"),
+            Student("Marco", "00000003", "電機系"), ]
+        self._rollcall_records = {
+            "1120518": RollcallRecord({
+                "00000001": 1,
+                "00000002": 1,
+                "00000003": 0,
+            }),
+            "1120525": RollcallRecord({
+                "00000001": 1,
+                "00000002": 0,
+                "00000003": 1,
+            })
+        }
 
     @property
     def name(self):
@@ -22,7 +36,7 @@ class Course:
     def get_student_list(self):
         return self._student_list
 
-    def add_rollcall_record(self, date, rollcall_result):
+    def add_rollcall_record(self, date, rollcall_result: dict()):
         self._rollcall_records[date] = RollcallRecord(rollcall_result)
 
     def add_student(self, name, ID, department):
