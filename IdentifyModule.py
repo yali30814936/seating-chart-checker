b504b96ae89b8291eb71fde6fcca2aa6bc961c29
UBIN <1213121abc@gmail.com>
2023-05-21 20:05:21 +0800

    feat: 辨識文字的配對演算法

--- IdentifyModule.py
+++ IdentifyModule.py
@@ -8,6 +8,10 @@ os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'source/seating-chart-checker-23
 
 
 def detect_text(imgPath):
+    """
+    return: a list of detected words
+    """
+
     client = vision.ImageAnnotatorClient()
 
     with open(imgPath, 'rb') as image_file:
@@ -18,25 +22,111 @@ def detect_text(imgPath):
     response = client.text_detection(image=image)
     texts = response.text_annotations
 
-    print('Texts:')
-    for text in texts:
-        print('\n"{}"'.format(text.description))
+    # print('Texts:')
+    # for text in texts:
+    #     print('\n"{}"'.format(text.description))
+    return texts[0].description.split('\n')
 
 
-def check_rollcall(imgPath, student_list):
-    result = dict()
+def check_rollcall(imgPath, student_list=[]):
+    """
+    student_list: currently not consider three-word name
+    return: a dict, key for all students in student_list, value for their attendance status (0:缺席 1:出席)
+    """
 
-    # 影像前處理
+    # --- 影像前處理 ---
 
-    detect_text(imgPath)
+    # --- 辨識文字 ---
 
-    # 辨識演算法
+    # word_list = detect_text(imgPath)
+    word_list = ['日期:QCT/5', '俞浩君', '某洢岑', '張文虹', '范文瑄', 'FIL', 'TTF', '講臺', '陳長', '戴柏儀', '劉明融', '姜紹淳', 'TOO', '技政偉孔繁道張慈芸大型陳慧慧', '劉品萱', '周远', '省達', '强思淇', '陈宇軒', '关系数', '天家', '陳芃铵', '黄雅欣', '駱宥亘|郭家佑']
+    print(word_list)
 
-    return result
+    # --- 配對演算法 ---
+
+    unmatched_students = set(student_list)
+    unmatched_words = set(word_list)
+
+    # # --- 完全相等的 先配對 ---
+    target = unmatched_students
+    to_add, to_remove = set(), set()
+    for word in unmatched_words:
+        if len(word) < 3:
+            continue
+        c = 0
+        while c < len(word)-2:
+            if word[c:c+3] in target:
+                unmatched_students.remove(word[c:c+3])
+                to_remove.add(word)
+                # 若此為長字串，就以該三字做分割，切出兩個字串，空字串就不用切出來了
+                if c != 0:
+                    to_add.add(word[:c])
+                if c+3 != len(word):
+                    to_add.add(word[c+3:])
+                c += 3
+            else:
+                c += 1
+    for ele in to_add:
+        unmatched_words.add(ele)
+    for ele in to_remove:
+        unmatched_words.remove(ele)
+    # print(unmatched_words)
+
+    # # --- 兩個字相同且其相對位置相同的 再配對 ---
+    target = dict()
+    for name in unmatched_students:
+        target[name[0:2] + '.'] = target[name[0] + '.' + name[2]] = target['.' + name[1:3]] = name
+    # print(target)
+    to_add, to_remove = set(), set()
+    for word in unmatched_words:
+        if len(word) == 1:
+            continue
+        if len(word) == 2:
+            for mask in [word + '.', '.' + word]:
+                if mask in target:
+                    unmatched_students.remove(target[mask])
+                    to_remove.add(word)
+                    break
+            continue
+        c = 0
+        while c < len(word) - 2:
+            for i, mask in enumerate([word[c:c+2] + '.', word[c] + '.' + word[c+2], '.' + word[c+1:c+3]]):
+                if mask in target:
+                    unmatched_students.remove(target[mask])
+                    to_remove.add(word)
+                    # 如果 '.' 不是夾在中間，就只要以兩字做分割，否則一樣三字
+                    # 但若 '.' 的那側只剩它一個字了，代表那個字大概只是辨識錯字，就不用切出來了
+                    if c != 0:
+                        to_add.add(word[:c+1 if i == 2 else c])
+                    if c + 3 != len(word):
+                        to_add.add(word[c+2 if i == 0 else c+3:])
+                    c += 2 if (i == 0 and c + 3 != len(word)) else 3
+                    break
+            else:
+                c += 1
+    for ele in to_add:
+        unmatched_words.add(ele)
+    for ele in to_remove:
+        unmatched_words.remove(ele)
+    print(unmatched_words)
+
+    # # --- 配對剩餘的 最好標示給使用者檢查 ---
+
+    # # --- 將沒配對上的框起來 ---
+
+    # --- 回傳出勤記錄 ---
+    attendance = dict()
+    for student in student_list:
+        attendance[student] = 0 if student in unmatched_students else 1
+
+    return attendance
 
 
 if __name__ == '__main__':
-    detect_text('source/sheet_samples/1.jpg')
+    student_list = ['俞浩君', '葉洢岑', '劉昀融', '張慈芸', '周子丞']
+    attendance_record = check_rollcall('source/sheet_samples/1.jpg', student_list)
+    print(attendance_record)
+
 
 # if __name__ == '__main__':
 #     # 指定剛剛下載tesseract.exe的完整路徑
